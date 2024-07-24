1df16068ce0e0affa26cdf23c741be5cb58594c3
Sathurjan <sathuplus@gmail.com>
2024-07-24 13:24:22 +0530

    added

---
+++ precache-manifest.6953cc9948fb52ba946013622261995a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55598e3566a380c53e14abafd7d6204a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb9c937772852a4d01a",
+    "url": "/static/css/4.34e73f49.chunk.css"
+  },
+  {
+    "revision": "53c92b7088d654c81fa2",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "aeb9c937772852a4d01a",
+    "url": "/static/js/4.dcf86ee0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/4.dcf86ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782e13a4f274f13ad609",
+    "url": "/static/js/5.74c2e613.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/5.74c2e613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d42e6d67c5e7acdc3c0",
+    "url": "/static/js/6.abc996bd.chunk.js"
+  },
+  {
+    "revision": "53c92b7088d654c81fa2",
+    "url": "/static/js/main.88d549ca.chunk.js"
+  },
+  {
+    "revision": "e70a2c75921c00da2a5f",
+    "url": "/static/js/runtime-main.4829df24.js"
+  },
+  {
+    "revision": "3f62a7c6c7dc236612a5",
+    "url": "/static/js/stencil-h8k-logo_2-entry-js.d0fe2f71.chunk.js"
+  },
+  {
+    "revision": "78940ecf094cf4e2253b",
+    "url": "/static/js/stencil-polyfills-css-shim.479b3246.chunk.js"
+  }
+]);
